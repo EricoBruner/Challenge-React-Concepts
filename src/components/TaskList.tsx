fdd362fe7043c1ab341338c50e102b952a31ee69
ERICO DA SILVA BRUNER <erico.bruner@redeunifique.com.br>
2022-04-20 19:54:24 -0300

    challenge finished

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,28 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle) {
+      const newTask = {
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete: false
+      }
+      setTasks((oldState:Task[]) => [...oldState, newTask])
+      setNewTaskTitle('')
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newArrayTasks = tasks.map((task:Task) => task.id == id ? {
+      ...task, 
+      isComplete: !task.isComplete
+    } : task)
+    setTasks(newArrayTasks)
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter((task:Task) => task.id != id)
+    setTasks(filteredTasks)
   }
 
   return (
